3fd3e2cd5f9c0c8ab7a10c1a8a00a42d4f14dd55
rottango <137510609+rottango@users.noreply.github.com>
2026-08-01 18:44:44 +0000

    wip: freaking rewriting all that shi cray cray cWAZY

--- include/node_class.hpp
+++ include/node_class.hpp
@@ -6,34 +6,50 @@
 #include <color_palet_struct.hpp>
 #include <opencv2/opencv.hpp>
 
-// observer
-// target
-// Only the target’s position and the observer’s position and heading matter
-
 class Node
 {
 public:
-    uint8_t node_id;
-
-    // cartesian
-    cv::Point2d cartesian_x_y_point;
-    cv::Point2d cartesian_x_axis_point;
-    cv::Point2d cartesian_y_axis_point;
-
-    // not specified
-    double theta_rotation_degrees;
-
-    // misc
-    colorPalet nodeColorPalet;
-
-    bool is_active;
-
     Node(uint8_t node_id, cv::Point2d cartesian_x_y_point,
          double theta_rotation_degrees,
          double vehicle_size_,
          double inner_,
          double measurment_error_CM_,
          colorPalet nodeColorPalet);
+
+private:
+    uint8_t getNodeId();
+
+    cv::Point2d getXYPoint();
+
+    cv::Point2d getXAxisPoint();
+
+    cv::Point2d getYAxisPoint();
+
+    bool getActiveState();
+
+    double getVehicleSize();
+
+    double getInner();
+
+    double getMeasurmentError();
+
+    void setNodeId();
+
+    void setXYPoint();
+
+    void setXAxisPoint();
+
+    void setYAxisPoint();
+
+    void setActiveState();
+
+    void setThetaRotationDegrees();
+
+    void setVehicleSize(double new_vehicle_size_);
+
+    void setInner(double new_inner_);
+
+    void setMeasurmentError(double new_measurment_error_CM_);
 
     void changePosition(cv::Point2d opencv_x_y_point);
 
@@ -43,20 +59,14 @@ public:
 
     void change_theta_rotation_degrees(double new_theta_rotation_degrees);
 
-    void setVehicleSize(double new_vehicle_size_);
-
-    void setInner(double new_inner_);
-
-    void setMeasurmentError(double new_measurment_error_CM_);
-
-    double getVehicleSize();
-
-    double getInner();
-
-    double getMeasurmentError();
-
-private:
+    uint8_t node_id_;
+    cv::Point2d cartesian_x_y_point_;
+    cv::Point2d cartesian_x_axis_point_;
+    cv::Point2d cartesian_y_axis_point_;
     int vehicle_size_;
     int inner_;
     int measurment_error_CM_;
+    double theta_rotation_degrees_;
+    colorPalet node_color_palet_;
+    bool is_active_;
 };
